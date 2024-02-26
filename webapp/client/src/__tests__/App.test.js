fd291280364da97fffc15eca9a472f608798621b
sergey <sergey@home.my>
2024-02-26 22:24:44 +0300

    Refactoring

--- webapp/client/src/__tests__/App.test.js
+++ webapp/client/src/__tests__/App.test.js
@@ -1,9 +1,11 @@
 import {render, screen} from '@testing-library/react';
 import App from '../App';
 
-test('renders loading element', () => {
-    // eslint-disable-next-line react/react-in-jsx-scope
-    render(<App />);
-    const loadingElement = screen.getByText(/Loading.../i);
-    expect(loadingElement).toBeInTheDocument();
+describe('App render', () => {
+    it('loading element', () => {
+        // eslint-disable-next-line react/react-in-jsx-scope
+        render(<App />);
+        const loadingElement = screen.getByText(/Loading.../i);
+        expect(loadingElement).toBeInTheDocument();
+    });
 });

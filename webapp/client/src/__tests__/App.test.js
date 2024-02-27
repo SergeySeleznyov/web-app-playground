2546cc6ee76a79c8821f85ac89267de13cf5b0f5
sergey <sergey@home.my>
2024-02-27 20:53:05 +0300

    Front: globaly mock fetch for test environment

--- webapp/client/src/__tests__/App.test.js
+++ webapp/client/src/__tests__/App.test.js
@@ -6,6 +6,16 @@ import App from '../App';
 
 describe('App render', () => {
     it('loading element', () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+            Promise.resolve({
+                // json: () => Promise.resolve([]),
+                json: () => new Promise((res, rej) => window.setTimeout(() => {
+                    rej(new Error('Postponed request has failted by timeout after test has already passed'));
+                }, 3000)),
+                status: 200,
+            }),
+        );
+
         // eslint-disable-next-line react/react-in-jsx-scope
         render(<App />);
         const loadingElement = screen.getByText(/Loading.../i);
--- webapp/client/src/setupTests.js
+++ webapp/client/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+global.fetch = (url) => {
+    const errorMessage = `Unexpected network usage from the test environment (${url})`;
+    console.error(errorMessage);
+    throw new Error(errorMessage);
+};
